95d9f0444f2fe853d9682b1d06dff1a7c916daf7
bachlang0000-sys <bachlang0000@gmail.com>
2026-07-23 19:15:59 +0700

    Cập nhật index.js kết nối Discord

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 require("dotenv").config();
 
 const { Client, GatewayIntentBits } = require("discord.js");
+const config = require("./config.json");
+
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
 });
 
+
 client.once("ready", () => {
-  console.log(`✅ Bot đã đăng nhập: ${client.user.tag}`);
+
+    console.log("--------------------------------");
+    console.log("🚑 EMS BOT ONLINE");
+    console.log(`Bot: ${client.user.tag}`);
+    console.log(`Server ID: ${config.serverId}`);
+    console.log("--------------------------------");
+
 });
+
+
+client.on("interactionCreate", async interaction => {
+
+    if (!interaction.isChatInputCommand()) return;
+
+
+    if(interaction.commandName === "vaoca"){
+
+        await interaction.reply({
+            content:"🚑 EMS: Bắt đầu ca trực!",
+            ephemeral:true
+        });
+
+    }
+
+
+    if(interaction.commandName === "roitca"){
+
+        await interaction.reply({
+            content:"🚑 EMS: Kết thúc ca trực!",
+            ephemeral:true
+        });
+
+    }
+
+
+});
+
 
 client.login(process.env.DISCORD_TOKEN);
